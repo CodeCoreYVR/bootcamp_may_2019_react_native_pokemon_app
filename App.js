2f6c108097eee9fa60764794dd4ebd2da5907d20
Brandon Y Lam <me@brandonlam.ca>
2020-08-31 10:16:49 -0700

    initialize & test React Native

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.h1}>Pokédex</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Hello World</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    backgroundColor: 'red',
+    paddingTop: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%'
+  },
+  h1: {
+    fontSize: 40
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  body: {
+    flex: 5
+  }
 });
