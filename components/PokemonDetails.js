49f454a67b4ea2789cc36ccdf70e591343476075
Brandon Y Lam <me@brandonlam.ca>
2020-09-01 11:12:15 -0700

    add ability to filter through images (with hacky solution)

--- components/PokemonDetails.js
+++ components/PokemonDetails.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, Button } from 'react-native';
 
 export default function PokemonDetails(props) {
   const url = props.route.params.pokemon.url
 
   const [pokemon, setPokemon] = useState({});
+  const [currentSprite, setCurrentSprite] = useState(0);
 
   useEffect(() => {
     fetch(url)
@@ -14,13 +15,35 @@ export default function PokemonDetails(props) {
       })
   }, [])
 
+  function updateDisplayedSprite(bool) {
+    if (bool) {
+      setCurrentSprite((state) => {
+        if (state === 7) {
+          return 0;
+        }
+        return state + 1;
+      })
+    } else {
+      setCurrentSprite((state) => {
+        if ( state === 0 ) {
+          return Object.keys(pokemon.sprites).length - 3
+        }
+        return state - 1;
+      })
+    }
+  }
+
   return(
     <View>
       <View style={styles.container}>
         {
           pokemon.id ? (
             <>
-              <Image style={styles.image} source={{ uri: pokemon.sprites.front_default }}/>
+              <View style={styles.imageContainer}>
+                <Button title='👈' onPress={() => updateDisplayedSprite(false) }></Button>
+                <Image style={styles.image} source={{ uri: pokemon.sprites[Object.keys(pokemon.sprites)[currentSprite]] || '#' }}/>
+                <Button title='👉' onPress={() => updateDisplayedSprite(true) }></Button>
+              </View>
               <Text style={styles.h1}>{pokemon.name}</Text>
               <Text style={styles.p}>Weight: {pokemon.weight}</Text>
               <Text style={styles.p}>Height: {pokemon.height}</Text>
@@ -36,6 +59,10 @@ export default function PokemonDetails(props) {
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imageContainer: {
+    flexDirection: 'row',
     alignItems: 'center'
   },
   image: {
